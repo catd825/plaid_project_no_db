1ec9c7343cb73fe27acb339aee07f9d99c49ee1e
catd825 <catd825@gmail.com>
2021-01-12 14:35:41 -0500

    updated server

--- server/index.js
+++ server/index.js
@@ -9,13 +9,6 @@ app.use(cors())
 app.use(bodyParser.json());
 
 
-const client = new plaid.Client({
-    clientID: keys.PLAID_CLIENT_ID,
-    secret: keys.PLAID_SECRET,
-    env: plaid.environments.sandbox
-  });
-
-
 
 const PORT = 5000;
 
