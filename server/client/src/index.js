eae8a9afd9e08905b47f9fa26acfe94c1d5ecf7b
catd825 <catd825@gmail.com>
2021-01-12 17:55:51 -0500

    updated imports in index.js client

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <div>
+      <App />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
 
